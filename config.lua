7e3a923e1b49d5fb7d888110d58c86dedb41c132
Putukasuma21 <kasuma.ananda1998@hotmail.com>
2021-09-09 23:55:12 +0800

    #1

---
+++ config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+Config.DrawDistance = 10
+Config.MarkerSize = {x = 0.5, y = 0.5, z = 0.5}
+Config.MarkerColor = {r = 255, g = 255, b = 255}
+Config.RoomMenuMarkerColor = {r = 255, g = 255, b = 255}
+Config.MarkerType = 21
+
+Config.RentModifier = 200 -- rent price: <property price> / <rent modifier> (rounded)
+Config.SellModifier = 2   -- sell price: <property price> / <sell modifier> (rounded)
+
+Config.Properties = {}
+
+Config.EnablePlayerManagement = false -- If set to true you use esx_realestateagentjob
+Config.Locale = 'en'
